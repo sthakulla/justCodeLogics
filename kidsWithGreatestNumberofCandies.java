c9734a9a859906c4a4fc3abc489aa1e86714b891
sthakulla <sthakull@uci.edu>
2021-08-19 23:08:25 -0700

    first completed

--- kidsWithGreatestNumberofCandies.java
+++ kidsWithGreatestNumberofCandies.java
@@ -1,6 +1,25 @@
 public class kidsWithGreatestNumberofCandies{
-    public List<Boolean> kdCandy(int[] candies, int extraCandies){
+    private int findMax(int[] can){
+        int res=0;
+        for(int i: can){
+            res=Math.max(res, i);
+        }
+        return res;
+    }
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n= candies.length-1;
+        int max=findMax(candies);
+        List<Boolean> res=new ArrayList<>();
+        for(int i: candies){
+            if((i+extraCandies)<max){
+                res.add(false);
+            }
+            else{
+                res.add(true);
+            }
 
+        }
+        return res;
     }
     public static void main(String[] args)
     {
