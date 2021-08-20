18f06971b3739747b4ccfafb669e878141c05b71
sthakulla <sthakull@uci.edu>
2021-08-19 22:56:24 -0700

    adding a little

---
+++ kidsWithGreatestNumberofCandies.java
@@ -0,0 +1,11 @@
+public class kidsWithGreatestNumberofCandies{
+    public List<Boolean> kdCandy(int[] candies, int extraCandies){
+
+    }
+    public static void main(String[] args)
+    {
+        int []candies={};
+        int extra=0;
+        kdCandy(candies, extra);
+    }
+}
